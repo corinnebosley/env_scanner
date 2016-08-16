2e7f45f62deaaac8537928d3e6a25dd80a6442dd
Corinne Bosley <corinne.bosley@metoffice.gov.uk>
2016-08-16 14:55:56 +0100

    hierarchy added to toy and toy test

--- testing/toy_test.py
+++ testing/toy_test.py
@@ -1,26 +1,31 @@
 ### MY TOY TEST FILE ###
 
+# This is an small example of bottom-up integration testing.
+
 import toy
 import unittest
 
 class TestMyFunction(unittest.TestCase):
+    # Start with some unit tests for the basic functionality
     def test_add(self):
-        self.assertEqual(toy.MyFunction(1, 2).add(), 3)
+        self.assertEqual(toy.MyFunction(1., 2.).add(), 3.)
 
     def test_subtract(self):
-        self.assertEqual(toy.MyFunction(6, 2).subtract(), 4)
+        self.assertEqual(toy.MyFunction(6., 2.).subtract(), 4.)
 
     def test_multiply(self):
-        self.assertEqual(toy.MyFunction(3, 3).multiply(), 9)
+        self.assertEqual(toy.MyFunction(3., 3.).multiply(), 9.)
 
-    def test_all(self):
-        numbers = toy.MyFunction(2, 4)
-        x = numbers.add()
-        y = numbers.subtract()
-        self.assertEqual(toy.MyFunction(x, y).multiply(), -12)
+    def test_divide(self):
+        self.assertEqual(toy.MyFunction(3., 3.).divide(), 1.)
 
     def test_nan(self):
         self.assertRaises(ValueError, toy.MyFunction, 'one', 'two')
+
+    # Now check that the hierarchy works properly
+    def test_bh(self):
+        self.assertEqual(toy.BlackHole(500., 1000.).bh_mass(),
+                         0.00037458795325142344)
 
 
 if __name__ == '__main__':
