c40ac1fe2ef1776b40fd49ff0bfe23de9f7b7d2a
Corinne Bosley <corinne.e.bosley@gmail.com>
2019-03-26 15:50:50 +0000

    added tests that work, but fail

--- lib/env_scanner/tests/unit/test_scan.py
+++ lib/env_scanner/tests/unit/test_scan.py
@@ -1,35 +1,39 @@
-import os
-import subprocess
 import pytest
-
-
-# class TestListContent():
-#     def setup_class(self):
-#         # Command to list available environments
-#         # get_envs = 'env_scanner --list_envs'
-#         self.env_list = subprocess.run(['env_scanner --list_envs'])
-#         assert subprocess.CompletedProcess.returncode == 0
-#         self.output = subprocess.check_output('env_scanner --list_envs',
-#                                               stderr=subprocess.STDOUT,
-#                                               shell=True)
-#         assert len(self.output) > 0
-#         assert len(self.output) > 1
-#
-#
-#     def test_list_content_all_envs(self):
-#         command_list = ['env_scanner --list_content -env '
-#                         '{}'.format(env) for env in self.output]
-#         subprocess.run([command for command in command_list],
-#                        capture_output=True)
-#         print(command_list)
-#         assert subprocess.CompletedProcess.returncode == 0
-
+from env_scanner.scan import list_contents, list_envs, find_pkg
 
 
 class TestListContent():
-    def test_list_content(self):
+    def test_experimental_current(self):
+        result = list_contents('scitools/experimental-current')
+        assert len(result) > 0
 
+    def test_experimental_legacy_current(self):
+        result = list_contents('scitools/experimental_legacy-current')
+        assert len(result) > 0
 
+    def test_default_next(self):
+        result = list_contents('scitools/default-next')
+        assert len(result) > 0
+
+    def test_default_legacy_next(self):
+        result = list_contents('scitools/default_legacy-next')
+        assert len(result) > 0
+
+    def test_default_current(self):
+        result = list_contents('scitools/default-current')
+        assert len(result) > 0
+
+    def test_default_legacy_current(self):
+        result = list_contents('scitools/default_legacy-current')
+        assert len(result) > 0
+
+    def test_default_previous(self):
+        result = list_contents('scitools/default_previous')
+        assert len(result) > 0
+
+    def test_default_legacy_previous(self):
+        result = list_contents('scitools/default_legacy-previous')
+        assert len(result) > 0
 
 if __name__ == '__main__':
     pytest.main()
